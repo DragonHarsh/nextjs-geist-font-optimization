ed2d831f31cd78fe838782f32ce117804ff62b40
DragonHarsh <81356149+DragonHarsh@users.noreply.github.com>
2025-07-23 17:09:14 +0530

    Add .next/static/chunks/src_app_page_tsx_269f1c1c._.js 1753270751618

---
+++ .next/static/chunks/src_app_page_tsx_269f1c1c._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_dd7b4181._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_27804559._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_547832d8._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_929a2e62._.js"
+],
+    source: "dynamic"
+});
